40b60dc885d9e6b5cadb20d01320bcdfb88f3cb4
Sebastian Diaz <seadiaz@gmail.com>
2018-04-20 11:49:29 -0300

    feat(receiver): add package

--- boot.go
+++ boot.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/segurosfalabella/imperium-worker/connection"
+	"github.com/segurosfalabella/imperium-worker/receiver"
 )
 
 var addr = flag.String("addr", "127.0.0.1:7700", "imperium server address")
@@ -21,9 +22,11 @@ func (s websocketDialerShim) Dial(urlStr string) (connection.WsConn, error) {
 
 func main() {
 	flag.Parse()
-	_, err := connection.Create(*addr, new(websocketDialerShim))
+	conn, err := connection.Create(*addr, new(websocketDialerShim))
 
 	if err != nil {
 		log.Println(err.Error())
 	}
+
+	receiver.Start(conn)
 }
---
+++ receiver/main.go
@@ -0,0 +1,8 @@
+package receiver
+
+import "github.com/segurosfalabella/imperium-worker/connection"
+
+// Start function
+func Start(conn connection.WsConn) {
+
+}
